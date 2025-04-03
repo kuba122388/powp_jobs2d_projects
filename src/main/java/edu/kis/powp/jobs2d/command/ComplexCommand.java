c3b729f41551ad83f94d552f33d082c3a9b67df6
JuliuszZolich <245971@edu.p.lodz.pl>
2025-04-03 13:00:04 +0200

    Implementation of ICompoundCommand.

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,31 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Implementation of Job2dDriverCommand for operateTo command functionality.
+ */
+public class ComplexCommand implements ICompoundCommand {
+
+    private final List<DriverCommand> commandList = new ArrayList<>();
+
+    public void addCommand(DriverCommand command) {
+        this.commandList.add(command);
+    }
+
+    @Override
+    public void execute(Job2dDriver driver){
+        while (this.iterator().hasNext()){
+            this.iterator().next().execute(driver);
+        }
+    }
+
+    @Override
+    public Iterator<DriverCommand> iterator() {
+        return commandList.iterator();
+    }
+}
