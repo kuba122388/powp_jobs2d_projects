c6d10570e6a7d7ff25e05d4391d0a9366050fc68
MichalC <m.colautti@o2.pl>
2025-04-08 18:52:52 +0200

    Repaired transformation decorator

--- src/main/java/edu/kis/powp/jobs2d/transformations/RotateTransformationDecorator.java
+++ src/main/java/edu/kis/powp/jobs2d/transformations/RotateTransformationDecorator.java
@@ -1,20 +1,18 @@
 package edu.kis.powp.jobs2d.transformations;
+import edu.kis.powp.jobs2d.Job2dDriver;
 
-import edu.kis.legacy.drawer.panel.DrawPanelController;
-import edu.kis.powp.jobs2d.drivers.TransformationDriver;
-
-public class RotateTransformationDriver extends TransformationDriver {
+public class RotateTransformationDecorator extends TransformationDecorator {
     private final double cos, sin;
 
-    public RotateTransformationDriver(DrawPanelController drawPanelController, double degrees) {
-        super(drawPanelController);
+    public RotateTransformationDecorator(Job2dDriver driver, double degrees) {
+        super(driver);
         double radians = Math.toRadians(degrees);
         this.cos = Math.cos(radians);
         this.sin = Math.sin(radians);
     }
 
     @Override
-    protected int[] applyTransformations(int x, int y) {
+    protected int[] transformation(int x, int y) {
         return new int[]{
                 (int) Math.round(x * cos - y * sin),
                 (int) Math.round(x * sin + y * cos)
